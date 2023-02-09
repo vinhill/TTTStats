13d258d32485b3d21f904654fc0315edf8c29f34
Vincent Hilla <vincenthilla5@gmail.com>
2023-02-09 13:34:21 +0100

    data table consistent with remaining containers

--- TTTStats/src/app/data-table/data-table.component.ts
+++ TTTStats/src/app/data-table/data-table.component.ts
@@ -1,37 +1,19 @@
-import { Component, Input, OnChanges } from '@angular/core';
-import { DataStoreService } from '../data-store.service';
-import { Dataframe } from '../dataframe';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
-export class DataTableComponent implements OnChanges {
-  loaded: boolean = false;
-  _result: any[] = [];
-  
-  @Input() query!: string;
-  @Input() params: any = {};
+export class DataTableComponent {
+  @Input() data!: any[];
   @Input() headers: {[key: string] : string} = {};
   @Input("columns") _columns: string = "";
 
   get columns() {
-    if (this._columns == "" && this._result.length > 0)
-      return Object.keys(this._result[0]);
+    if (this._columns == "" && this.data.length > 0)
+      return Object.keys(this.data[0]);
     else
       return this._columns.split(",").map(s => s.trim());
-  }
-
-  constructor(private datastore: DataStoreService) { }
-
-  ngOnChanges() {
-    this.load();
-  }
-
-  async load() {
-    this.loaded = false;
-    this._result = await this.datastore.get(this.query, this.params);
-    this.loaded = true;
   }
 }
